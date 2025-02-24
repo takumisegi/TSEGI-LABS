361476611c3f860c4052dc4ec07cde73590322f1
takumisegi <takumi.segi@gmail.com>
2025-02-24 14:39:13 -0500

    Code error fixed
    error was made because of extra #

--- lab1/galaga_01_IfElse.gd
+++ lab1/galaga_01_IfElse.gd
@@ -12,18 +12,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
-		#position.x += speed.
+		position.x += speed
 	if Input.is_action_pressed("ui_left"):
-		#position.x += speed.
+		position.x += -speed
 	if Input.is_action_pressed("ui_up"):
-		#position.y += speed.
+		position.y += -speed
 	if Input.is_action_pressed("ui_down"):
-		#position.y += speed.
+		position.y += speed
 	
 		#if Input . is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 			#position.x += 1.
-		
-		
 		
 		
 		#if health < 0:
