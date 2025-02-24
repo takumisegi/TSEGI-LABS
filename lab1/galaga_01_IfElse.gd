292f8dbc88ba85d8222ee52bdeb81a65bb3ec32b
takumisegi <takumi.segi@gmail.com>
2025-02-24 14:27:00 -0500

    added code for each direciton

--- lab1/galaga_01_IfElse.gd
+++ lab1/galaga_01_IfElse.gd
@@ -2,6 +2,9 @@ extends Sprite2D
 
 @export var health = 10
 @export var booleanVar = true
+
+@export var speed = 5
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -9,10 +12,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
-		#position.x += 1.
+		#position.x += speed.
+	if Input.is_action_pressed("ui_left"):
+		#position.x += speed.
+	if Input.is_action_pressed("ui_up"):
+		#position.y += speed.
+	if Input.is_action_pressed("ui_down"):
+		#position.y += speed.
 	
-	#if Input . is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		#position.x += 1.
+		#if Input . is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			#position.x += 1.
 		
 		
 		
