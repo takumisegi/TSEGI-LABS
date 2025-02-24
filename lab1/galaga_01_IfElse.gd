ab1d7e4e761bba86e5830fa7098364126e4f51a2
takumisegi <takumi.segi@gmail.com>
2025-02-24 14:24:21 -0500

    lab 2 - but i have errors
    can not figure out indent

---
+++ lab1/galaga_01_IfElse.gd
@@ -0,0 +1,25 @@
+extends Sprite2D
+
+@export var health = 10
+@export var booleanVar = true
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("ui_right"):
+		#position.x += 1.
+	
+	#if Input . is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		#position.x += 1.
+		
+		
+		
+		
+		#if health < 0:
+			#hide()
+		#else:
+			#show()
+		
+		
